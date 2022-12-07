59917d78bc7f56927c927c8c84674ee6a7e8d0fa
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-08 00:35:33 +0530

    initial

---
+++ src/main/java/com/nest/shoppingcartnew_backend/ShoppingcartnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.shoppingcartnew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartnewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/shoppingcartnew_backend/ShoppingcartnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.shoppingcartnew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
